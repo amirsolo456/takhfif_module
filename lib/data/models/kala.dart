a6985394078c46a09a43ed4192c230a85cf67c89
Amir <amirsol456@gmail.com>
2026-08-30 03:09:06 +0330

    Fix Kala model to match KianStore API

--- lib/data/models/kala.dart
+++ lib/data/models/kala.dart
@@ -1,5 +1,5 @@
 class Kala {
-  final int id;
+  final String id;
   final String code;
   final String name;
   final int? typeId;
@@ -8,6 +8,7 @@ class Kala {
   final bool isActive;
   final double? purchasePrice;
   final double? salePrice;
+  final double? stock;
   final String? description;
 
   Kala({
@@ -20,20 +21,31 @@ class Kala {
     this.isActive = true,
     this.purchasePrice,
     this.salePrice,
+    this.stock,
     this.description,
   });
 
   factory Kala.fromJson(Map<String, dynamic> json) {
+    final id = (json['id'] ?? json['code'] ?? '').toString();
+    final name = (json['name'] ?? json['kalaName'] ?? '').toString();
+    final sale = json['salePrice'] ?? json['mabFrosh'];
+    final purchase = json['purchasePrice'] ?? json['mabKharid'];
+
     return Kala(
-      id: json['id'],
-      code: json['code'],
-      name: json['name'],
-      typeId: json['typeId'],
-      unitId: json['unitId'],
+      id: id,
+      code: (json['code'] ?? json['id'] ?? '').toString(),
+      name: name,
+      typeId: json['typeId'] ?? json['kalaType'],
+      unitId: json['unitId'] ?? json['idSanjesh'],
       barcode: json['barcode'],
-      isActive: json['isActive'] ?? true,
-      purchasePrice: json['purchasePrice'] != null ? (json['purchasePrice'] as num).toDouble() : null,
-      salePrice: json['salePrice'] != null ? (json['salePrice'] as num).toDouble() : null,
+      isActive: !(json['isDisabled'] ?? false),
+      purchasePrice: purchase is num ? purchase.toDouble() : null,
+      salePrice: sale is num ? sale.toDouble() : null,
+      stock: json['stock'] is num
+          ? (json['stock'] as num).toDouble()
+          : json['quantity'] is num
+              ? (json['quantity'] as num).toDouble()
+              : null,
       description: json['description'],
     );
   }
