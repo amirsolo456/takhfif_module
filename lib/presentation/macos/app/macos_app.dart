7585067b05191852afe15d1f1ff023cb2b42c1b0
Amir <amirsol456@gmail.com>
2026-09-04 16:26:12 +0330

    Apply unified professional theme to macOS UI

--- lib/presentation/macos/app/macos_app.dart
+++ lib/presentation/macos/app/macos_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import '../../../core/theme/app_theme.dart';
 import '../../windows/pages/windows_discount_home_page.dart';
 
 class MacOSApp extends StatelessWidget {
@@ -8,21 +9,15 @@ class MacOSApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'مدیریت تخفیف (macOS)',
+      title: 'مدیریت تخفیف',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-        useMaterial3: true,
-        fontFamily: 'Tahoma',
-      ),
+      theme: AppTheme.light,
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('fa', 'IR'),
-      ],
+      supportedLocales: const [Locale('fa', 'IR')],
       locale: const Locale('fa', 'IR'),
       home: const WindowsDiscountHomePage(),
     );
