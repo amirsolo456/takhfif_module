fad1db77266fc42ba89d9d3516c8a6fb2b0d9e0d
Amir <amirsol456@gmail.com>
2026-09-06 09:52:31 -0700

    Set Kavenegar sender line

--- lib/core/config/kavenegar_config.dart
+++ lib/core/config/kavenegar_config.dart
@@ -8,7 +8,7 @@ class KavenegarConfig {
 
   // Kavenegar sender line configured for this account.
   static const String _envSender = String.fromEnvironment('KAVENEGAR_SENDER');
-  static const String defaultSender = '10004346';
+  static const String defaultSender = '0018018949161';
 
   static String get sender => _envSender.isNotEmpty ? _envSender : defaultSender;
 
