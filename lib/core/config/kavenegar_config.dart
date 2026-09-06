cc99701e9a73ad739cf5480b543c9bf814fcb643
Amir <amirsol456@gmail.com>
2026-09-06 09:44:32 -0700

    Set verified Kavenegar sender line

--- lib/core/config/kavenegar_config.dart
+++ lib/core/config/kavenegar_config.dart
@@ -2,12 +2,15 @@ class KavenegarConfig {
   const KavenegarConfig._();
 
   static const String _envApiKey = String.fromEnvironment('KAVENEGAR_API_KEY');
-  static const String defaultApiKey = '6A596E4A70744252764A4A36546F4A75724334754C62366E436C677839653855614F63386149452F3943383D';
+  static const String defaultApiKey = String.fromEnvironment('KAVENEGAR_API_KEY');
 
   static String get apiKey => _envApiKey.isNotEmpty ? _envApiKey : defaultApiKey;
 
-  // Optional Kavenegar sender/line. Leave empty to use the account default line.
-  static const String sender = String.fromEnvironment('KAVENEGAR_SENDER');
+  // Kavenegar sender line verified by the user.
+  static const String _envSender = String.fromEnvironment('KAVENEGAR_SENDER');
+  static const String defaultSender = '09192410207';
+
+  static String get sender => _envSender.isNotEmpty ? _envSender : defaultSender;
 
   static const String apiBaseUrl = 'https://api.kavenegar.com/v1';
 }
