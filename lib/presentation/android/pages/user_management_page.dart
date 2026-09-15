2e1f75087b86dfb66398c6eb7a19beb220fc1fa6
Amir <amirsol456@gmail.com>
2026-09-15 04:31:19 -0700

    Add user management page

---
+++ lib/presentation/android/pages/user_management_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class UserManagementPage extends StatelessWidget {
+  const UserManagementPage({super.key});
+  @override
+  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('مدیریت کاربران')));
+}
