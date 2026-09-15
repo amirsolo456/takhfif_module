f0e30314e58896f042ae7c36695bc98b8dedbd9d
Amir <amirsol456@gmail.com>
2026-09-15 04:26:09 -0700

    Add persisted login gate

---
+++ lib/presentation/android/pages/auth_gate.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../../data/repositories/auth_repository.dart';
+import 'login_page.dart';
+import 'main_navigation_page.dart';
+
+class AuthGate extends StatefulWidget {
+  final AuthRepository authRepository;
+  const AuthGate({super.key, required this.authRepository});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  late Future<bool> _sessionFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _sessionFuture = _hasSession();
+  }
+
+  Future<bool> _hasSession() async {
+    final token = await widget.authRepository.getToken();
+    return token != null && token.isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _sessionFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        if (snapshot.data == true) return const MainNavigationPage();
+        return LoginPage(authRepository: widget.authRepository);
+      },
+    );
+  }
+}
