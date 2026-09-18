6c71f56da8c69c136711d30bb7d93da7b08ec387
Amir <amirsol456@gmail.com>
2026-09-18 01:40:32 -0700

    security(sms): do not provide embedded API key fallback

--- lib/core/config/kavenegar_config.dart
+++ lib/core/config/kavenegar_config.dart
@@ -4,7 +4,7 @@ class KavenegarConfig {
   static const String _envApiKey = String.fromEnvironment('KAVENEGAR_API_KEY');
   static const String defaultApiKey = '';
 
-  static String get apiKey => _envApiKey.isNotEmpty ? _envApiKey : defaultApiKey;
+  static String get apiKey => _envApiKey;
 
   // Kavenegar sender line shown in the account's Java web-service example.
   static const String _envSender = String.fromEnvironment('KAVENEGAR_SENDER');
